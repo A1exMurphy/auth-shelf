33f420f30b5dc60985bbb8051a02504c7e507307
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-12-14 13:56:39 -0600

    made add and delete saga functions

--- src/redux/sagas/shelf.saga.js
+++ src/redux/sagas/shelf.saga.js
@@ -11,23 +11,58 @@ function* fetchItems() {
     };
 
     // the config includes credentials which
-    // allow the server session to recognize the Items
-    // If a Items is logged in, this will return their information
-    // from the server session (req.Items)
+    // allow the server session to recognize the user
+    // If a user is logged in, this will return their information
+    // from the server session (req.user)
     const response = yield axios.get('/api/shelf', config);
     console.log(response.data, "this is the response from GET");
 
-    // now that the session has given us a Items object
-    // with an id and Itemsname set the client-side Items object to let
-    // the client-side code know the Items is logged in
+    // now that the session has given us a user object
+    // with an id and username set the client-side user object to let
+    // the client-side code know the user is logged in
     yield put({ type: 'SET_ITEMS', payload: response.data });
   } catch (error) {
     console.log('Items GET request failed', error);
   }
 }
+// addItem will take an (action) because it will have a payload to send
+function* addItem(action) {
+  // const config = {
+  //   headers: { 'Content-Type': 'application/json' },
+  //   withCredentials: true,
+  // };
+  try { 
+    axios({
+      method: 'POST',
+      url: `/api/shelf/${action.payload.id}`,
+      data: action.payload
+    })
+    yield put({
+      type: 'FETCH_Items'
+    })
+  } catch(error) {
+    console.log('Error in POST route', error)
+  }
+}
+
+function* deleteItem(action) {
+  try{
+    axios({
+      method: 'DELETE',
+      url: `/api/shelf/${action.payload}`
+    }) 
+    yield put({
+      type: 'FETCH_Items'
+    })
+  } catch(error) {
+    console.log('Error in POST route', error)
+  }
+}
 
 function* itemsSaga() {
   yield takeLatest('FETCH_Items', fetchItems);
+  yield takeLatest('ADD_item', addItem);
+  yield takeLatest('DELETE_item', deleteItem);
 }
 
 export default itemsSaga;
