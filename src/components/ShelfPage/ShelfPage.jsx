1e01f499e2c98c329b13186738c9650d7d3c5e19
Abubakar Aden <abubakar.aden978@gmail.com>
2023-12-14 14:19:11 -0600

    Added the form useState and created dispatch func

--- src/components/ShelfPage/ShelfPage.jsx
+++ src/components/ShelfPage/ShelfPage.jsx
@@ -1,41 +1,54 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import React from 'react';
+import React from "react";
 
 function ShelfPage() {
-  const shelfItems = useSelector(store => store.shelf)
+  const shelfItems = useSelector((store) => store.shelf);
   // console.log(shelfItems, typeof shelfItems, "shelfItems from store")
   const dispatch = useDispatch();
 
   useEffect(() => {
-    fetchItems()
+    fetchItems();
   }, []);
 
   const fetchItems = () => {
     // console.log("dispatch FETCH_Items")
     dispatch({
-      type: "FETCH_Items" 
-  });   
+      type: "FETCH_Items",
+    });
+  };
+  const [description, setDescription] = useState("");
+  const [imageLink, setImageLink] = useState("");
 
-  }
+  const addItem = (e) => {
+    e.preventDefault();
+    //  function is sending the new item details to the store..
+    dispatch({
+      type: "ADD_item",
+      payload: { description: description, image_url: imageLink },
+    });
+  };
+
   return (
     <div className="container">
       <h2>Shelf</h2>
       <form>
-        <input type="text" placeholder='Enter item description'/>
-        <input type="text"  placeholder='Paste image link'/>
-        <button>Add Item</button>
+        <input type="text" placeholder="Enter item description" />
+        <input type="text" placeholder="Paste image link" />
+        <button onClick={(e) => addItem(e)}>Add Item</button>
       </form>
       <ul>
-      {shelfItems && shelfItems.map((item) => {
-        return(
-          <div key={item.id} className='itemObject'>
-            <li>{item.description}</li>
-            <img src={item.image_url} />
-          </div>
-        )
-      })}</ul>
+        {shelfItems &&
+          shelfItems.map((item) => {
+            return (
+              <div key={item.id} className="itemObject">
+                <li>{item.description}</li>
+                <img src={item.image_url} />
+              </div>
+            );
+          })}
+      </ul>
       <p>{}</p>
     </div>
   );
