5c924c5db7c3b426c64bcb413ef2002258137528
Evan Langer <elanger@live.com>
2023-12-14 10:59:19 -0600

    made GET query in a shelf router

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -6,8 +6,19 @@ const router = express.Router();
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
-  res.sendStatus(200); // For testing only, can be removed
-});
+  const query = 
+    `
+    SELECT * FROM "item";
+    `
+    pool.query(query)
+    .then((result) => {
+      res.send(result.rows)
+    })
+    .catch((err) => {
+      console.log(err, "error in GET query")
+      res.sendStatus(500)
+    })
+    });
 
 /**
  * Add an item for the logged in user to the shelf
